c2b6479df184ea332fb1ccd324cb97e6d3a48093
kyasya <59965258+kyashaa@users.noreply.github.com>
2025-09-04 03:11:22 +0900

    Updating and backup
    Updated: pltstyle
    Developing: kplot.common, fitting

--- src/build/lib/kplot/kfittingbase.py
+++ src/build/lib/kplot/kfittingbase.py
@@ -8,8 +8,6 @@ from scipy.optimize import curve_fit
 
 from IPython.display import display
 
-import warnings
-
 # 作りかけ
 class kFittingBase:
 	fDataFrameStyle = True
@@ -22,11 +20,6 @@ class kFittingBase:
 	Conv = None
 	dfParamTable = None
 	def __init__(self):
-		warnings.warn(
-            f"{self.__class__.__name__} class is developing. Implemented functions may change without notice.",
-            category=UserWarning,
-            stacklevel=2
-        )
 		pass
 	def SetPointN(self, val):
 		self.Nsamples = val
--- src/kplot/pltstyle.py
+++ src/kplot/pltstyle.py
@@ -1,31 +1,108 @@
+#%%
+"""kplot.pltstyle
+
+* Optimizing default parameters in matplotlib for kplot.
+
+"""
+import platform
 import matplotlib as plt
 
-# font
-# plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["font.size"] = 16
+def SetDefaultFont(font='auto', font_size=20):
+	"""Font settings
 
-# axis
-plt.rcParams["xtick.direction"] = "in"
-plt.rcParams["ytick.direction"] = "in"
+	Set font type and size. default font set 'sans-serif'.
+	You can set to be serif-font, using 'paper' or 'auto' parameter.
 
-plt.rcParams["xtick.minor.visible"] = True
-plt.rcParams["ytick.minor.visible"] = True
+	Args:
+		font (str): Set font type or tag ['paper', 'auto'].
+		font_size (int): Font size
+	"""
 
-plt.rcParams["xtick.major.width"] = 1.0
-plt.rcParams["ytick.major.width"] = 1.0
+	Font='sans-serif'
+	System = platform.system()
 
-plt.rcParams["xtick.major.size"] = 10
-plt.rcParams["ytick.major.size"] = 10
+	if font == 'paper': font = 'auto'
 
-plt.rcParams["xtick.minor.width"] = 1.0
-plt.rcParams["ytick.minor.width"] = 1.0
+	if font!='': 
+		if font=='auto':
+			if System in ("Windows", "Darwin"):
+				Font='Times New Roman'
+			else: Font='Nimbus Roman'
+		else: Font=font
 
-plt.rcParams["xtick.minor.size"] = 5
-plt.rcParams["ytick.minor.size"] = 5
+	# print(Font)
+	plt.rcParams["font.family"] = Font
+	plt.rcParams["font.size"] = font_size
 
-# ticks
-plt.rcParams['xtick.top'] = False
-plt.rcParams['ytick.right'] = False
+def SetLoadLatex(flag=True):
+	"""LoadLatex 
+		If this flag set to be True, This function load Installed LaTeX system. 
+	"""
+	plt.rcParams["text.usetex"] = flag
 
-# Frame
-plt.rcParams["axes.linewidth"] = 1.0
+
+def SetDefaultFigSize(tag:str='default', fig_size:list=[], x=8, y=6, dpi=350):
+	"""SetDefaultFigSize
+
+	Settings for default figure size. Default set a figure size to 8 inch x 6 inch.
+
+	Args:
+		tag (str): set figure size using tag: 'default', 'A4', 'a4', 'B5', 'b5'.
+		fig_size (list): set figure size using list: [x, y]
+		x, y (float): set figure size
+		dpi (float): set figure resolution (dpi).
+	"""
+	FigSize=[8, 6]
+	if x != 8: FigSize[0]=x
+	if y != 6: FigSize[1]=y
+	if fig_size != []:
+		if len(fig_size) != 2:
+			print('[WARNING:SetDefaultFigSize] List length of figure size parameters can set to be 2.')
+			print(f'[WARNING:SetDefaultFigSize] You set {fig_size}. It\'s ignored.')
+		else: FigSize=fig_size
+	if tag in ['A4', 'a4']: FigSize = [11.69, 8.27]
+	if tag in ['B5', 'b5']: FigSize = [10.10, 7.20]
+
+	plt.rcParams["figure.figsize"] = FigSize
+	plt.rcParams["savefig.dpi"] = dpi
+
+def SetDefaultSettings():
+	"""SetDefaultSettings
+	Load and set to default figure setting parameters.
+	"""
+	SetDefaultFont(font='auto', font_size=20)
+
+	SetDefaultFigSize('A4', dpi=350)
+
+	# axis
+	plt.rcParams["xtick.direction"] = 'in'
+	plt.rcParams["ytick.direction"] = 'in'
+
+	plt.rcParams["xtick.minor.visible"] = True
+	plt.rcParams["ytick.minor.visible"] = True
+
+	plt.rcParams["xtick.major.width"] = 1.0
+	plt.rcParams["ytick.major.width"] = 1.0
+
+	plt.rcParams["xtick.major.size"] = 10
+	plt.rcParams["ytick.major.size"] = 10
+
+	plt.rcParams["xtick.minor.width"] = 1.0
+	plt.rcParams["ytick.minor.width"] = 1.0
+
+	plt.rcParams["xtick.minor.size"] = 5
+	plt.rcParams["ytick.minor.size"] = 5
+
+	# ticks
+	plt.rcParams['xtick.top'] 	= False
+	plt.rcParams['ytick.right'] = False
+
+	# Frame
+	plt.rcParams["axes.linewidth"] = 1.0
+
+	# plt.rcParams["lines.marker"] = 'o'
+	plt.rcParams["lines.color"] = 'black'
+
+# plt.rcParamsDefault.keys()
+SetDefaultSettings()
+
